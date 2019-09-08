736ac9b68da3c3cb06a87d2d353e29c182a44f37
zyggggggggg <1820784487@qq.com>
2019-09-08 10:16:37 +0800

    计数器基本结构完成

---
+++ src/components/Addition.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <p>当前最新的值为：{{$store.state.count}}</p>
+        <button>+1</button>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
